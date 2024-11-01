2c1bb84d416c7d351b00a92211ebe600c3d6ecba
INV8 <chaiyapat.sr@ku.th>
2024-11-01 16:15:14 +0700

    update recently repository

--- src/main/java/ku/cs/task_management/repositories/RecentlyRepository.java
+++ src/main/java/ku/cs/task_management/repositories/RecentlyRepository.java
@@ -1,6 +1,7 @@
 package ku.cs.task_management.repositories;
 
 import ku.cs.task_management.entities.RecentlyView;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -9,9 +10,9 @@ import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface RecentlyRepository {
+public interface RecentlyRepository extends JpaRepository<RecentlyView, UUID> {
     @Query(
-            value = "SELECT r FROM RecentlyView r WHERE r.recentViewer.memberId = :id"
+            value = "SELECT r FROM RecentlyView r WHERE r.recentViewer.memberId = :id ORDER BY r.recentlyTime"
     )
     List<RecentlyView> findByMemberId(@Param("id")UUID id);
 }
