fe0631aec172dbb37d79c20d5166369e7ec48fe6
INV8 <chaiyapat.sr@ku.th>
2024-11-01 21:53:28 +0700

    fix bug

--- src/main/java/ku/cs/task_management/responses/RecentlyViewResponse.java
+++ src/main/java/ku/cs/task_management/responses/RecentlyViewResponse.java
@@ -1,6 +1,5 @@
 package ku.cs.task_management.responses;
 
-import ku.cs.task_management.commons.RecentlyType;
 import ku.cs.task_management.entities.RecentlyView;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,22 +11,11 @@ import java.util.UUID;
 public class RecentlyViewResponse {
     private UUID recentlyViewId;
     private String recentlyViewName;
-    private RecentlyType recentlyType;
     private UUID viewId;
 
     public RecentlyViewResponse(RecentlyView r) {
         this.recentlyViewId = r.getRecentlyId();
-        switch (r.getRecentlyType()) {
-            case PROJECT :
-                this.recentlyType = RecentlyType.PROJECT;
-                this.recentlyViewName = r.getRecentlyProject().getProjectName();
-                this.viewId = r.getRecentlyProject().getProjectId();
-                break;
-            case TASK :
-                this.recentlyType = RecentlyType.TASK;
-                this.recentlyViewName = r.getRecentlyTask().getTaskName();
-                this.viewId = r.getRecentlyTask().getTaskId();
-                break;
-        }
+        this.recentlyViewName = r.getRecentlyProject().getProjectName();
+        this.viewId = r.getRecentlyProject().getProjectId();
     }
 }
